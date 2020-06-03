8dd828da99c638528bc62f597f2be1e321079f3f
oppaiman22 <iqbalnurimansyah@gmail.com>
2020-06-04 03:32:50 +0700

    pengen bubukkkkk

--- src/tictactoe/view/GamePanel.java
+++ src/tictactoe/view/GamePanel.java
@@ -3,22 +3,39 @@ package tictactoe.view;
 import java.awt.BasicStroke;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import tictactoe.controller.GameController;
 import tictactoe.model.GameProperty;
-import tictactoe.resources.Resource;
 
 public class GamePanel extends JPanel {
 
     private GameController gameController;
     private GameProperty gameProperty;
+    private static Image[] letters;
     
     public GamePanel(GameController gameController) {
         gameProperty = new GameProperty();
         this.gameController = gameController;
         addMouseListener(new Input());
+        letters = new Image[2];
+        letters[0] = loadImage("/blueX.png");
+        letters[1] = loadImage("/resources/blueCircle.png");
+    }
+    
+    private BufferedImage loadImage(String path) {
+        try {
+            return ImageIO.read(this.getClass().getResource(path));
+        } catch (IOException exception) {
+            exception.printStackTrace();
+            System.exit(-1);
+        }
+        return null;
     }
             
     @Override
@@ -36,12 +53,12 @@ public class GamePanel extends JPanel {
             graphics2D.drawLine(0, x, gameProperty.getWIDTH(), x);
         }
         
-        Resource resource = new Resource();
+        
         for(int x = 0; x < 3; x++) {
             for(int y = 0; y < 3; y++) {
                 int field = gameController.getFields()[x][y];
                 if(field != gameProperty.getNOBODY())
-                    graphics2D.drawImage(resource.letters[field - 1], 
+                    graphics2D.drawImage(letters[field - 1], 
                                          x * gameProperty.getFIELD_WIDTH(), 
                                          y * gameProperty.getFIELD_HEIGHT(), 
                                          gameProperty.getFIELD_WIDTH(), 
